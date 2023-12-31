d4193c3094ddcec4d561a492434ed06a41657641
SamarV-121 <samar@samarv121.dev>
2023-12-31 18:07:10 +0530

    gerrit-utils: review: Allow appending multiple options
    Change-Id: I92e43c34d6ceba9e8167944aeb3b870631614b37

--- gerrit/actions.py
+++ gerrit/actions.py
@@ -3,17 +3,20 @@ from gerrit.api import get_changes_list
 
 def review(ssh, args):
     changes = get_changes_list(ssh, args, action="review")
+    review_query = []
 
     if args.abandon:
-        review_query = "--abandon"
-    elif args.code_review:
-        review_query = f"--code-review {args.code_review}"
-    elif args.verified:
-        review_query = f"--verified {args.verified}"
+        review_query.append("--abandon")
+
+    if args.code_review:
+        review_query.append(f"--code-review {args.code_review}")
+
+    if args.verified:
+        review_query.append(f"--verified {args.verified}")
 
     for change in changes:
-        print(f"{args.review}: {change}")
-        command = f"gerrit review {review_query} {change}"
+        command = f"gerrit review {' '.join(review_query)} {change}"
+        print(command)
         ssh.exec_command(command)
 
 
